33045d728827a13effd9ade8512f444f79233d97
Andrew Yankovsky <yandrew91@gmail.com>
2013-01-08 10:33:12 +0300

    Home screen added

--- chaplin-lambda-banking-client/js/user/routes.js
+++ chaplin-lambda-banking-client/js/user/routes.js
@@ -4,8 +4,8 @@ define(function() {
     // The routes for the application. This module returns a function.
     // `match` is match method of the Router
     var routes = function(match) {
-        match('', 'cards#index');
-        match('index', 'cards#index');
+        match('', 'home#index');
+        match('index', 'home#index');
 
         match('cards', 'cards#index');
 
